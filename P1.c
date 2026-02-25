318a4013923c859b8824281c1e364a0e327cacf8
bansarirami <bansarirami1@gmail.com>
2026-02-25 21:25:59 +0530

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+
+{
+    float a ,b;
+    printf("enter value of a & b");
+    scanf("%f %f", &a, &b);
+    printf("addition is %f \n", a+b);
+    printf("substitution is %f \n", a-b);
+    printf("multiplication is %f \n", a*b);
+    printf("division is %f \n", a/b);
+    
+    return 0; 
+}
